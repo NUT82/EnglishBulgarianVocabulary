c4e217c3cda1e99cd5ecd4a84fbdfffded7fe6e5
Svetoslav <s.petrov.bg@gmail.com>
2021-03-25 19:50:26 +0200

    Add project files.

---
+++ DataBaseVocabulary/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataBaseVocabulary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
